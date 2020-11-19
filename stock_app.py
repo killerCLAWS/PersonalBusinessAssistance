a2c86aac07a6d997c62390da6a93566fa768e99e
Ankit Jaiswal <ankit.aj9935@gmail.com>
2020-11-19 11:22:25 +0530

    Dashboard Made to display on web

--- stock_app.py
+++ stock_app.py
@@ -4,49 +4,75 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+
+
 app = dash.Dash()
 server = app.server
+
 scaler=MinMaxScaler(feature_range=(0,1))
-df_nse = pd.read_csv("./NSE-TATA.csv")
+
+
+
+df_nse = pd.read_csv("./NSE-Tata-Global-Beverages-Limited.csv")
+
 df_nse["Date"]=pd.to_datetime(df_nse.Date,format="%Y-%m-%d")
 df_nse.index=df_nse['Date']
+
+
 data=df_nse.sort_index(ascending=True,axis=0)
 new_data=pd.DataFrame(index=range(0,len(df_nse)),columns=['Date','Close'])
+
 for i in range(0,len(data)):
     new_data["Date"][i]=data['Date'][i]
     new_data["Close"][i]=data["Close"][i]
+
 new_data.index=new_data.Date
 new_data.drop("Date",axis=1,inplace=True)
+
 dataset=new_data.values
+
 train=dataset[0:987,:]
 valid=dataset[987:,:]
+
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(dataset)
+
 x_train,y_train=[],[]
+
 for i in range(60,len(train)):
     x_train.append(scaled_data[i-60:i,0])
     y_train.append(scaled_data[i,0])
     
 x_train,y_train=np.array(x_train),np.array(y_train)
+
 x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
 model=load_model("saved_model.h5")
+
 inputs=new_data[len(new_data)-len(valid)-60:].values
 inputs=inputs.reshape(-1,1)
 inputs=scaler.transform(inputs)
+
 X_test=[]
 for i in range(60,inputs.shape[0]):
     X_test.append(inputs[i-60:i,0])
 X_test=np.array(X_test)
+
 X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 closing_price=model.predict(X_test)
 closing_price=scaler.inverse_transform(closing_price)
+
 train=new_data[:987]
 valid=new_data[987:]
 valid['Predictions']=closing_price
+
+
+
 df= pd.read_csv("./stock_data.csv")
+
 app.layout = html.Div([
    
     html.H1("Stock Price Analysis Dashboard", style={"textAlign": "center"}),
@@ -54,48 +80,54 @@ app.layout = html.Div([
     dcc.Tabs(id="tabs", children=[
        
         dcc.Tab(label='NSE-TATAGLOBAL Stock Data',children=[
-            html.Div([
-                html.H2("Actual closing price",style={"textAlign": "center"}),
-                dcc.Graph(
-                    id="Actual Data",
-                    figure={
-                        "data":[
-                            go.Scatter(
-                                x=train.index,
-                                y=valid["Close"],
-                                mode='markers'
-                            )
-                        ],
-                        "layout":go.Layout(
-                            title='scatter plot',
-                            xaxis={'title':'Date'},
-                            yaxis={'title':'Closing Rate'}
-                        )
-                    }
-                ),
-                html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
-                dcc.Graph(
-                    id="Predicted Data",
-                    figure={
-                        "data":[
-                            go.Scatter(
-                                x=valid.index,
-                                y=valid["Predictions"],
-                                mode='markers'
-                            )
-                        ],
-                        "layout":go.Layout(
-                            title='scatter plot',
-                            xaxis={'title':'Date'},
-                            yaxis={'title':'Closing Rate'}
-                        )
-                    }
-                )                
-            ])                
+			html.Div([
+				html.H2("Actual closing price",style={"textAlign": "center"}),
+				dcc.Graph(
+					id="Actual Data",
+					figure={
+						"data":[
+							go.Scatter(
+								x=train.index,
+								y=valid["Close"],
+								mode='markers'
+							)
+
+						],
+						"layout":go.Layout(
+							title='scatter plot',
+							xaxis={'title':'Date'},
+							yaxis={'title':'Closing Rate'}
+						)
+					}
+
+				),
+				html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
+				dcc.Graph(
+					id="Predicted Data",
+					figure={
+						"data":[
+							go.Scatter(
+								x=valid.index,
+								y=valid["Predictions"],
+								mode='markers'
+							)
+
+						],
+						"layout":go.Layout(
+							title='scatter plot',
+							xaxis={'title':'Date'},
+							yaxis={'title':'Closing Rate'}
+						)
+					}
+
+				)				
+			])        		
+
+
         ]),
         dcc.Tab(label='Facebook Stock Data', children=[
             html.Div([
-                html.H1("Facebook Stocks High vs Lows", 
+                html.H1("Stocks High vs Lows", 
                         style={'textAlign': 'center'}),
               
                 dcc.Dropdown(id='my-dropdown',
@@ -107,7 +139,7 @@ app.layout = html.Div([
                              style={"display": "block", "margin-left": "auto", 
                                     "margin-right": "auto", "width": "60%"}),
                 dcc.Graph(id='highlow'),
-                html.H1("Facebook Market Volume", style={'textAlign': 'center'}),
+                html.H1("Stocks Market Volume", style={'textAlign': 'center'}),
          
                 dcc.Dropdown(id='my-dropdown2',
                              options=[{'label': 'Tesla', 'value': 'TSLA'},
@@ -120,8 +152,17 @@ app.layout = html.Div([
                 dcc.Graph(id='volume')
             ], className="container"),
         ])
+
+
     ])
 ])
+
+
+
+
+
+
+
 @app.callback(Output('highlow', 'figure'),
               [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown):
@@ -157,6 +198,8 @@ def update_graph(selected_dropdown):
                    'rangeslider': {'visible': True}, 'type': 'date'},
              yaxis={"title":"Price (USD)"})}
     return figure
+
+
 @app.callback(Output('volume', 'figure'),
               [Input('my-dropdown2', 'value')])
 def update_graph(selected_dropdown_value):
@@ -186,5 +229,8 @@ def update_graph(selected_dropdown_value):
                    'rangeslider': {'visible': True}, 'type': 'date'},
              yaxis={"title":"Transactions Volume"})}
     return figure
+
+
+
 if __name__=='__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True)
